1122ff4df8cc40b09d685c0b42f2c001c1bdb0a3
Jason Zhang <workmail.jason@foxmail.com>
2017-11-25 10:25:31 +0800

    Update utils.py

--- BinaryClassification/utils.py
+++ BinaryClassification/utils.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+ #coding:utf-8
 import numpy as np
 from scipy.stats import mode 
 # 根据经纬度计算距离
@@ -22,24 +22,51 @@ def mode_function(df):
     counts = mode(df) 
     return counts[0][0] / 10000
 
+def bearing_array(lat1, lng1, lat2, lng2):
+    """ function was taken from beluga's notebook as this function works on array
+    while my function used to work on individual elements and was noticably slow"""
+    AVG_EARTH_RADIUS = 6371  # in km
+    lng_delta_rad = np.radians(lng2 - lng1)
+    lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
+    y = np.sin(lng_delta_rad) * np.cos(lat2)
+    x = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(lng_delta_rad)
+    return np.degrees(np.arctan2(y, x))
+
 
 def get_features_list():
-
-	features = [
+    features = [
                 # 原始特征 0 - 4
                 'bsCount','category_id','mall_id','price','connect',
+                # 偏差特征 5 - 10 
+                'c_wifi_var','wifi_var','angle_var','sb_wifi_var','s_wifi_var','c_sb_wifi_var',
+                # 'sb_history_avg_power',c_sb_history_avg_power'
                 # 时间特征 11
                 'current_hour',
-                # 范围 12 - 15
-                'distance','s_median_scale',
+                # 范围角度特征 12 - 15
+                'distance','s_median_scale','current_bearing_array','history_bearing_array_median',
                 # 统计特征 16
                 'wifi_cover_shop',
                 # 比例特征 17 - 18
                 'shop_wifi_connect_ratio','tfid_features',
+                # 其他特征 19 - 20
+                'nature_order',
+                'w_std_power',
+                # 
+                's_sb_wifi_var_ratio',
+                # 时间历史热度
+                # '1_shop_hot','3_shop_hot', '5_shop_hot', '7_shop_hot'
+
+                #我们自己添加的feature
+                'p_avg_price',
+                'p_shop_favor',
+                's_hot',
+                'r_mean',
+                'wifirank_diff',
+                's_conncectwifi_count'
 
                 ]
       
-	return features
+    return features
 
 def time_map(time):
     time = int(time)
